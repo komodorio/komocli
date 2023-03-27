bb1190a8b6f986a94380389f2dc2a333b3056773
Andrei Pohilko <andrei.pokhilko@gmail.com>
2023-03-27 16:15:57 +0100

    Working state, needs polishing

--- pkg/portforward/main.go
+++ pkg/portforward/main.go
@@ -6,12 +6,25 @@ import (
 	log "github.com/sirupsen/logrus"
 	"net"
 	"os"
+	"strconv"
 )
 
 func RunPortForwarding(ctx context.Context, agentId string, ns string, pod string, rPort int, lport int, jwt string) error {
-	// test connect to Komodor WS endpoint
-	ws := NewWSConnectionWrapper(ctx, nil, agentId, jwt, true)
-	ws.Run()
+	// template message for session starts
+	initMsg := &SessionMessage{
+		MessageType: MTPodExecInit,
+		Data: &PodExecInitData{
+			Namespace: ns,
+			PodName:   pod,
+			Cmd:       PortForwardCMDPrefix + strconv.Itoa(rPort),
+		},
+	}
+
+	err := testConnection(ctx, agentId, jwt, initMsg)
+	if err != nil {
+		return err
+	}
+	log.Infof("Finished testing the connectivity, ready to accept connections")
 
 	// check and bind local port, mind the host
 	host := os.Getenv("KOMOCLI_BIND")
@@ -22,10 +35,14 @@ func RunPortForwarding(ctx context.Context, agentId string, ns string, pod strin
 	if err != nil {
 		return err
 	}
-	defer listen.Close()
+
+	go func() {
+		<-ctx.Done()
+		listen.Close()
+	}()
 
 	// setup connection handler
-	go acceptIncomingConn(ctx, listen, agentId, jwt)
+	acceptIncomingConns(ctx, listen, agentId, jwt, initMsg)
 
 	<-ctx.Done() // chill on ctx
 
@@ -33,16 +50,41 @@ func RunPortForwarding(ctx context.Context, agentId string, ns string, pod strin
 	return nil
 }
 
-func acceptIncomingConn(ctx context.Context, listen net.Listener, agentId string, jwt string) {
+func testConnection(ctx context.Context, agentId string, jwt string, initMsg *SessionMessage) error {
+	// test connect to Komodor WS endpoint
+	ws := NewWSConnectionWrapper(ctx, nil, agentId, jwt, true, *initMsg)
+	err := ws.Run()
+	if err != nil {
+		log.Warnf("Failed to test port-forward operability: %+v", err)
+		return err
+	}
+
+	err = ws.Stop()
+	if err != nil {
+		log.Warnf("Failed to send session termination message: %s", err)
+		return err
+	}
+	return err
+}
+
+func acceptIncomingConns(ctx context.Context, listen net.Listener, agentId string, jwt string, initMsg *SessionMessage) {
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
-			log.Fatalf("Failed to accept incoming connection: %+v", err)
+			log.Warnf("Failed to accept incoming connection: %+v", err)
 			break
 		}
 
 		log.Infof("Accepted connection: %v", conn.LocalAddr())
-		ws := NewWSConnectionWrapper(ctx, conn, agentId, jwt, false)
-		go ws.Run()
+		ws := NewWSConnectionWrapper(ctx, conn, agentId, jwt, false, *initMsg)
+		go func() {
+			<-ctx.Done()
+			ws.Stop()
+		}()
+
+		go func() {
+			ws.Run()
+		}()
 	}
+	log.Infof("Stopped accepting incoming connections")
 }
--- pkg/portforward/ws.go
+++ pkg/portforward/ws.go
@@ -12,6 +12,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"time"
 )
 
 type WSConnectionWrapper struct {
@@ -21,15 +22,17 @@ type WSConnectionWrapper struct {
 	agentId    string
 	jwt        string
 	isConnTest bool
+	SessionId  string
+	initMsg    *SessionMessage
+
+	chReady chan struct{}
 }
 
-func (w *WSConnectionWrapper) Run() {
+func (w *WSConnectionWrapper) Run() error {
 	defer func() {
 		if !w.isConnTest {
 			log.Infof("Done working with connection: %v", w.tcpConn.LocalAddr())
 			_ = w.tcpConn.Close()
-		} else {
-			log.Infof("Finished testing the connectivity")
 		}
 	}()
 
@@ -52,54 +55,71 @@ func (w *WSConnectionWrapper) Run() {
 	w.wsConn, err = w.connectWS(url, hdr)
 	if err != nil {
 		log.Warnf("Failed to open WebSocket connection: %+v", err)
-		return
+		return err
 	}
 
 	// write initial msg
-	msg := SessionMessage{
-		SessionId:   PortForwardCMDPrefix + uuid.New().String(),
-		MessageId:   uuid.New().String(),
-		MessageType: MTPodExecInit,
-	}
+	w.initMsg.MessageId = uuid.New().String()
+	w.initMsg.Timestamp = time.Now()
 
-	txt, err := json.Marshal(msg)
+	err = w.sendWS(w.initMsg)
 	if err != nil {
-		log.Errorf("Failed to serialize output message: %s", err)
-		return
+		return err
 	}
 
-	err = w.wsConn.WriteMessage(websocket.TextMessage, txt)
-	if err != nil {
-		log.Errorf("Failed to send output message over WS: %s", err)
-		return
-	}
+	go func() {
+		<-w.ctx.Done()
+		if w.tcpConn != nil {
+			w.tcpConn.Close()
+		}
+	}()
 
-	// 		setup read/write loops
-	bridge := NewWSBridge(w.wsConn)
+	// write loop
 	go func() {
 		if w.isConnTest {
 			log.Debugf("Not trying to send data due to validation loop")
 			return
 		}
 
-		n, err := io.Copy(bridge, w.tcpConn)
+		log.Debugf("Starting tcp->ws transfer")
+		n, err := io.Copy(w, w.tcpConn)
 		log.Infof("Done tcp->ws transfer: %d bytes", n)
 		if err != nil {
 			log.Warnf("Problems transfering tcp->ws: %s", err)
 		}
 	}()
 
+	// read loop
 	var wr io.Writer
 	if w.isConnTest {
 		wr = &bytes.Buffer{}
 	} else {
 		wr = w.tcpConn
 	}
-	n, err := io.Copy(wr, bridge)
+	n, err := io.Copy(wr, w)
 	log.Infof("Done ws->tcp transfer: %d bytes", n)
 	if err != nil {
 		log.Warnf("Problems in ws->tcp transfer: %s", err)
+		return err
 	}
+
+	return nil
+}
+
+func (w *WSConnectionWrapper) sendWS(initMsg *SessionMessage) error {
+	txt, err := json.Marshal(initMsg)
+	if err != nil {
+		log.Errorf("Failed to serialize output message: %s", err)
+		return err
+	}
+
+	log.Debugf("Sending WS message: %s", txt)
+	err = w.wsConn.WriteMessage(websocket.TextMessage, txt)
+	if err != nil {
+		log.Errorf("Failed to send output message over WS: %s", err)
+		return err
+	}
+	return nil
 }
 
 func (w *WSConnectionWrapper) connectWS(url string, hdr http.Header) (*websocket.Conn, error) {
@@ -115,13 +135,96 @@ func (w *WSConnectionWrapper) connectWS(url string, hdr http.Header) (*websocket
 	return conn, nil
 }
 
-func NewWSConnectionWrapper(ctx context.Context, conn net.Conn, agentId string, jwt string, isConnTest bool) *WSConnectionWrapper {
+func (w *WSConnectionWrapper) Write(p []byte) (n int, err error) {
+	<-w.chReady // we need to wait for ack before writing anything
+
+	// we received data via TCP and now want to translate it into WS message
+	msg := SessionMessage{
+		MessageId:   uuid.New().String(),
+		SessionId:   w.SessionId,
+		MessageType: MTStdin,
+		Data: &PodExecStdinData{
+			Input: string(p),
+		},
+		Timestamp: time.Now(),
+	}
+
+	err = w.sendWS(&msg)
+	if err != nil {
+		return 0, err
+	}
+
+	// loop bridged messages
+	return len(p), err
+}
+
+func (w *WSConnectionWrapper) Read(p []byte) (n int, err error) {
+	_, bts, err := w.wsConn.ReadMessage()
+	if err != nil {
+		log.Warnf("Failed to read message from WS: %s", err)
+		return len(bts), err
+	}
+
+	log.Debugf("Read msg over WS: %s", bts)
+	var msg SessionMessage
+	err = json.Unmarshal(bts, &msg)
+	if err != nil {
+		return 0, err
+	}
+
+	if msg.MessageType == MTAck && msg.Data.(*PodExecAckData).AckedMessageID == w.initMsg.MessageId {
+		w.SessionId = msg.SessionId
+		close(w.chReady) // ready to write data into WS
+	}
+
+	if msg.MessageType == MTStdout {
+		payload := []byte(msg.Data.(*PodExecStdoutData).Out)
+		copy(p, payload) // FIXME: copy buffered!
+		n = len(payload)
+	} else if w.isConnTest && msg.MessageType == MTAck {
+		err = io.EOF // enough for connection test
+	} else if msg.MessageType == MTError {
+		err = fmt.Errorf("received error from remote: %s", msg.Data.(*PodExecErrorData).ErrorMessage)
+	}
+
+	return n, err
+}
+
+func (w *WSConnectionWrapper) Stop() error {
+	err := w.sendWS(&SessionMessage{
+		MessageId:   uuid.NewString(),
+		SessionId:   w.SessionId,
+		MessageType: MTTermination,
+		Data: &PodExecSessionTerminationData{
+			ProcessExitCode: 0,
+			ExitMessage:     "Stopping",
+		},
+		Timestamp: time.Now(),
+	})
+	if err != nil {
+		log.Debugf("Failed to send WS termination: %s", err)
+		return err
+	}
+
+	err = w.tcpConn.Close()
+	if err != nil {
+		log.Debugf("Failed to close connection: %s", err)
+		return err
+	}
+
+	return nil
+}
+
+func NewWSConnectionWrapper(ctx context.Context, conn net.Conn, agentId string, jwt string, isConnTest bool, initMsg SessionMessage) *WSConnectionWrapper {
 	return &WSConnectionWrapper{
 		ctx:        ctx,
 		tcpConn:    conn,
 		isConnTest: isConnTest,
+		initMsg:    &initMsg, // this is intentional to accept dereferenced value, to create a copy of it
 
 		agentId: agentId,
 		jwt:     jwt,
+
+		chReady: make(chan struct{}),
 	}
 }
