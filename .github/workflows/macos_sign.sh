ecf2bfd4dab044486c252c6784ea35cec4496684
Andrei Pohilko <andrei.pokhilko@gmail.com>
2023-06-18 14:50:37 +0100

    Try using zsign

--- .github/workflows/macos_sign.sh
+++ .github/workflows/macos_sign.sh
@@ -4,16 +4,17 @@
 echo $MACOS_CERTIFICATE_P12 | base64 --decode >certificate.p12
 
 # Create a keychain
-security create-keychain -p actions build.keychain
+# security create-keychain -p actions build.keychain
 
 # Import the certificate
-security import certificate.p12 -k build.keychain -P "" -T /usr/bin/codesign
+# security import certificate.p12 -k build.keychain -P "" -T /usr/bin/codesign
 
 # Remember the keychain
-security list-keychains -s build.keychain
+# security list-keychains -s build.keychain
 
 # Unlock the keychain
-security unlock-keychain -p actions build.keychain
+# security unlock-keychain -p actions build.keychain
 
 # Find the macOS binary and sign it
-codesign --force --sign "$CERTIFICATE_ID" --timestamp $1
+# TODO "$CERTIFICATE_ID"
+zsign --pkey certificate.p12 $1
