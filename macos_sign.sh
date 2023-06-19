7c4b34f7d77e252e261f03ab79df8f3dcf8d631a
Andrei Pohilko <andrei.pokhilko@gmail.com>
2023-06-19 19:04:07 +0100

    Test

--- macos_sign.sh
+++ macos_sign.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-export USERNAME="${{ secrets.APPLE_ID_USERNAME }}"
-export PASSWORD="${{ secrets.APP_SPECIFIC_PASSWORD }}"
 export ARTIFACT=$1
 envsubst < gon.hcl > gon_processed.hcl
 gon --log-level=DEBUG gon_processed2.hcl
