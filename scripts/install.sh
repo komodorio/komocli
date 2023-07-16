acea17793a781bc31a6bf770578db3994e3e2abf
guy93r <guy.rosen@komodor.io>
2023-07-16 13:47:48 +0300

    added install script

---
+++ scripts/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+get_os() {
+  case "$OSTYPE" in
+    linux*)   echo "linux";;
+    darwin*)  echo "darwin";;
+    win*)     echo "windows";;
+    *)        echo "unknown";;
+  esac
+}
+get_arch() {
+  uname -m
+}
+get_download_url() {
+  curl -s https://api.github.com/repos/komodorio/komocli/releases/latest \
+  | jq --arg platform "${os}_${arch}" -r '.assets[] | select(.browser_download_url | contains($platform)) | .browser_download_url'
+}
+
+os=$(get_os)
+arch=$(get_arch)
+download_url=$(get_download_url)
+
+echo $os
+echo $arch
+echo $download_url
+echo "Downloading komocli package..."
+curl -LO "$download_url"
+
+
+
+echo "Extracting komocli package..."
+tar -xf "komocli_0.0.3_${os}_${arch}.tar.gz"
+
+echo "Installing komocli..."
+sudo mv komocli /usr/local/bin/
+rm "komocli_0.0.3_${os}_${arch}.tar.gz"
+echo "komocli installation completed!"
