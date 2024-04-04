1e9a9bf9a2e0973e21c0488204a91f267bb9cd3a
Andrey Pokhilko <andrei@komodor.io>
2024-04-04 12:05:41 +0100

    Debug MacOS signing (#24)
    * Debug MacOS signing
    
    * More debug
    
    * Notarize binary
    
    * Zip before notarizing
    
    * Rollback unneeded

--- scripts/notarize-macos-app.sh
+++ scripts/notarize-macos-app.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 echo "Notarizing $1"
-xcrun notarytool submit $1 \
+zip $1.zip $1
+xcrun notarytool submit $1.zip \
     --apple-id "$MACOS_NOTARY_APPLE_ID" \
     --team-id "$MACOS_NOTARY_TEAM_ID" \
     --password "$MACOS_NOTARY_PASSWORD" \
-    --wait
+    --wait 2>&1 | tee -a notarytool.log
