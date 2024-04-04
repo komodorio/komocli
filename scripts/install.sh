c5d455d4e31d0a09d290dce17e1dc42fab27be14
Yossi Yadgar <yossi@komodor.io>
2024-04-04 16:14:08 +0300

    CU-86bx79rb6: Fix bash installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 get_os() {
-  case "$OSTYPE" in
-    linux*)   echo "linux";;
-    darwin*)  echo "darwin";;
-    win*)     echo "windows";;
-    *)        echo "unknown";;
-  esac
+    case "$OSTYPE" in
+        linux*)   echo "linux";;
+        darwin*)  echo "darwin";;
+        win*)     echo "windows";;
+        *)        echo "unknown";;
+    esac
 }
 get_arch() {
-  arch1=$(uname -m)
-  if [[ "$arch1" == "x86_64" ]]; then
-    arch1="amd64"
-  fi
-  echo $arch1
+    arch1=$(uname -m)
+    if [[ "$arch1" == "x86_64" ]]; then
+        arch1="amd64"
+    fi
+    echo $arch1
 }
 
 get_download_url() {
-  curl -s https://api.github.com/repos/komodorio/komocli/releases/latest \
-  | curl -s https://api.github.com/repos/komodorio/komocli/releases/latest | grep -o "https://.*${os}_${arch}\.tar\.gz"
+    curl -s https://api.github.com/repos/komodorio/komocli/releases/latest \
+    | curl -s https://api.github.com/repos/komodorio/komocli/releases/latest | grep -o "https://.*${os}_${arch}"
 }
 
 get_version() {
@@ -36,12 +36,7 @@ echo $download_url
 echo "Downloading komocli package..."
 curl -LO "$download_url"
 
-
-
-echo "Extracting komocli package..."
-tar -xf "komocli_${version}_${os}_${arch}.tar.gz"
-
 echo "Installing komocli..."
-sudo mv komocli /usr/local/bin/
-rm "komocli_${version}_${os}_${arch}.tar.gz"
+sudo mv "komocli_${version}_${os}_${arch}" /usr/local/bin/komocli
+chmod +x /usr/local/bin/komocli
 echo "komocli installation completed!"
