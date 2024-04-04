c75c7f1e150390f22e2ba771f9c06bfaef7f6fa5
Andrei Pohilko <andrei.pokhilko@gmail.com>
2024-04-04 11:14:44 +0100

    Debug MacOS signing

--- scripts/notarize-macos-app.sh
+++ scripts/notarize-macos-app.sh
@@ -4,4 +4,4 @@ xcrun notarytool submit $1 \
     --apple-id "$MACOS_NOTARY_APPLE_ID" \
     --team-id "$MACOS_NOTARY_TEAM_ID" \
     --password "$MACOS_NOTARY_PASSWORD" \
-    --wait
+    --wait 2>&1 | tee -a notarytool.log
