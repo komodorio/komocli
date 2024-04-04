6eb04c6e41f37e5dfa0da07f6e18af6988b7cd18
Andrei Pohilko <andrei.pokhilko@gmail.com>
2024-04-04 11:39:37 +0100

    Zip before notarizing

--- scripts/notarize-macos-app.sh
+++ scripts/notarize-macos-app.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 echo "Notarizing $1"
-xcrun notarytool submit $1 \
+zip $1.zip $1
+xcrun notarytool submit $1.zip \
     --apple-id "$MACOS_NOTARY_APPLE_ID" \
     --team-id "$MACOS_NOTARY_TEAM_ID" \
     --password "$MACOS_NOTARY_PASSWORD" \
