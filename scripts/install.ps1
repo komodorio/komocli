3c6c436161798d4aa84422cdf5c9b644954db362
Amit Bar Oz <amit@komodor.io>
2024-07-10 12:08:17 +0300

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -46,12 +46,12 @@ function AddTo-Path{
         [string]$Dir
     )
     if (!(Test-Path $Dir) ){
-        Write-warning "Supplied directory was not found!"
+        Write-Warning "Supplied directory was not found!"
         return
     }
-    $PATH = [Environment]::GetEnvironmentVariable("PATH", "Machine")
+    $PATH = [Environment]::GetEnvironmentVariable("PATH", "User")
     if ($PATH -notlike "*"+$Dir+"*" ){
-        [Environment]::SetEnvironmentVariable("PATH", "$PATH;$Dir", "Machine")
+        [Environment]::SetEnvironmentVariable("PATH", "$PATH;$Dir", "User")
     }
 }
 
